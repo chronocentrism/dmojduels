7bc06f88873733b53d25a0d0d83c83a38ed4fdd6
Spencer Yuan <aerialspy4@gmail.com>
2025-08-18 15:08:50 -0400

    Merge

--- proxy.js
+++
@@ -1,9 +0,0 @@
-const cors_proxy = require('cors-anywhere');
-
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: [],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(6969, '0.0.0.0', () => {
-    console.log('CORS Anywhere running on http://localhost:6969');
-});
--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const fs = require("fs")
 const path = require('path');
 
 
+// PROXY
+
+const cors_proxy = require('cors-anywhere');
+
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: [],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(6969, '0.0.0.0', () => {
+    console.log('CORS Anywhere running on http://localhost:6969');
+});
+
+
+
 
 // create new contest id
 
